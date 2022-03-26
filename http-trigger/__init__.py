927d29603ef91f909951912951b05ec21521de57
Chris Tomich <chris.tomich@gmail.com>
2022-03-26 12:15:35 +1100

    Added logging for debugging

--- http-trigger/__init__.py
+++ http-trigger/__init__.py
@@ -1,12 +1,18 @@
 import logging
 import azure.functions as func
 from weasyprint import HTML
+import hashlib
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    logging.info(list(req.headers.keys()))
+    logging.info(list(req.headers.values()))
+    logging.info('request body hash ' + hashlib.md5(req.get_body()).hexdigest())
 
     # name = req.params.get('name')
     pdf = HTML(string=req.get_body().decode('utf-8')).write_pdf()
 
-    return func.HttpResponse(pdf)
+    logging.info('response body hash ' + hashlib.md5(pdf).hexdigest())
+
+    return func.HttpResponse(pdf, mimetype='application/pdf')
